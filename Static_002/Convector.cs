b6c57e71d6201a53126b673bbeaff7d5b46230ec
Yaroslav <history.shots19@gmail.com>
2022-02-23 18:13:51 +0200

    fixed all comments

--- Static_002/Convector.cs
+++ Static_002/Convector.cs
@@ -4,15 +4,17 @@ namespace Static_002
 {
     static class Convector
     {
+        private const double firstFormulaCosntant = 1.8;
+        private const int secondFormulaCosntant = 32;
         public static double CelsiusToFahrenheit(string temperatureCelsius)
         {
             double tempCelsius = Convert.ToDouble(temperatureCelsius);
-            return tempCelsius * 1.8 + 32;
+            return tempCelsius * firstFormulaCosntant + secondFormulaCosntant;
         }
         public static double FahrenheitToCelsius(string temperatureFahrenheit)
         {
             double tempFahrenheit = Convert.ToDouble(temperatureFahrenheit);
-            return Math.Round((tempFahrenheit - 32) / 1.8 , 2); 
+            return Math.Round((tempFahrenheit - secondFormulaCosntant) / firstFormulaCosntant, 2); 
         }
     }
 }
